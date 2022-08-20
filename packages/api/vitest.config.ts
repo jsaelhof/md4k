2bebce99c0ec1c0730e7b0bc53d846ab0cfce0f3
Jason Saelhof <jsaelhof@gmail.com>
2022-08-20 00:30:50 -0600

    setup vitest

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -1,14 +1,13 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: "node",
   },
 });
