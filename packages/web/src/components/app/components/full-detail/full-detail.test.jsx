6a85c16dadb52aacc6a94ecb873cb2571c1d84c2
Jason Saelhof <jsaelhof@gmail.com>
2023-04-13 11:11:11 -1000

    #69: add mock intersection observer to full detail test

--- packages/web/src/components/app/components/full-detail/full-detail.test.jsx
+++ packages/web/src/components/app/components/full-detail/full-detail.test.jsx
@@ -7,6 +7,9 @@ import { renderWithProviders } from "../../../../utils/render-with-providers";
 import { createMatchMedia } from "../../../../utils/create-match-media";
 import { buildThirdPartyMovieMock } from "../../../../utils/build-third-party-movie-mock";
 import { GET_THIRD_PARTY_MOVIE_FULL_DETAILS } from "../../../../graphql/queries";
+import { mockInteresctionObserver } from "../../../../utils/mock-intersection-observer";
+
+mockInteresctionObserver();
 
 vi.mock("uuid", () => ({
   v4: () => "111-222-333",
