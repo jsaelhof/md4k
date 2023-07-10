f3fe791397d80aeebcffcb0fd4761ed52f3b2dd4
Jason Saelhof <jsaelhof@gmail.com>
2023-07-09 22:49:06 -0600

    #96: remove my useIntersectionObserver hook and use Rook's useInVIewRef. Fix tests and mocking.

--- packages/web/vitest-setup.js
+++ packages/web/vitest-setup.js
@@ -3,11 +3,10 @@ import { beforeAll, beforeEach, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 
 beforeAll(() => {
-  // Define a mock for intersection observer.
-  // Currently this is simply stubbing mock functions and is enough for my needs.
-  global.IntersectionObserver = vi.fn(() => ({
-    observe: vi.fn(),
-    unobserve: vi.fn(),
+  // Define a mock for Rooks useInViewRef hook.
+  // Currently this is simply returning a null ref and always returns true for intersecting.
+  vi.mock("rooks/dist/esm/hooks/useInViewRef", () => ({
+    useInViewRef: vi.fn().mockReturnValue([null, true]),
   }));
 });
 
