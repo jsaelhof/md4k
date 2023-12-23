ffd9f4866faf6cc8cab4e34b463936728eb79da4
Jason Saelhof <jsaelhof@gmail.com>
2023-12-22 21:11:11 -0700

    #150: add typescript linting to the web package and fix typing issues in app context

--- packages/web/src/context/app-context.tsx
+++ packages/web/src/context/app-context.tsx
@@ -11,8 +11,8 @@ export type AppContextType = {
   setList?: (list: GetListsItem) => void;
   movies: GetMovieItem[];
   moviesById: { [key: string]: GetMovieItem };
-  pick?: any;
-  setPick?: (v: any) => void;
+  pick: string | null;
+  setPick?: (value: string | null) => void;
   clearPick?: () => void;
   toast: Maybe<ToastProps>;
   setToast: (toast: ToastProps) => void;
@@ -23,7 +23,9 @@ const AppContext = createContext<AppContextType>({
   movies: [],
   moviesById: {},
   toast: null,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   setToast: () => {},
+  pick: null,
 });
 
 const AppProvider = ({ children }: PropsWithChildren) => {
@@ -34,8 +36,9 @@ const AppProvider = ({ children }: PropsWithChildren) => {
   // It complets after the network part of cache-and-network is done so its late if there is cached data available. We want to take advantage of that to load really fast.
   const { movies, moviesById } = useGetMovies(list ?? lists?.[0]);
   const [toast, setToast] = useState<ToastProps | null>(null);
-  const [pick, setPick] = useState(null);
+  const [pick, setPick] = useState<string | null>(null);
 
+  console.log({ pick });
   // Expose a list change function so that we can clear any state from the old list while changing to a new one
   const setList = useCallback((list: GetListsItem) => {
     setPick(null);
@@ -46,125 +49,6 @@ const AppProvider = ({ children }: PropsWithChildren) => {
     setPick(null);
   }, []);
 
-  /**
-   * {
-    "lists": [
-        {
-            "__typename": "List",
-            "id": "5ea3b2ee-761b-47c8-abb9-9377f3826931",
-            "label": "Staging"
-        }
-    ],
-    "list": {
-        "__typename": "List",
-        "id": "5ea3b2ee-761b-47c8-abb9-9377f3826931",
-        "label": "Staging"
-    },
-    "movies": [
-        {
-            "__typename": "Movie",
-            "id": "0b3c9190-f6d9-404b-904f-654a127a4ae7",
-            "title": "Star Wars: Episode IV - A New Hope",
-            "list": "5ea3b2ee-761b-47c8-abb9-9377f3826931",
-            "runtime": 7260,
-            "source": 6,
-            "genre": 3,
-            "year": "1977",
-            "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-            "imdbID": "tt0076759",
-            "locked": false,
-            "addedOn": "2021-12-29T19:55:11.013Z",
-            "watchedOn": null,
-            "ratings": {
-                "__typename": "Ratings",
-                "id": "0b3c9190-f6d9-404b-904f-654a127a4ae7",
-                "IMDB": "86%",
-                "ROTTEN_TOMATOES": "92%",
-                "METACRITIC": "90%"
-            },
-            "background": null
-        },
-        {
-            "__typename": "Movie",
-            "id": "66974fab-db07-41d9-8ecc-f4ceabd891f9",
-            "title": "The Goonies",
-            "list": "5ea3b2ee-761b-47c8-abb9-9377f3826931",
-            "runtime": 6840,
-            "source": 0,
-            "genre": 1,
-            "year": "1985",
-            "poster": "https://m.media-amazon.com/images/M/MV5BODRlMjRkZGEtZWM2Zi00ZjYxLWE0MWUtMmM1YWM2NzZlOTE1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-            "imdbID": "tt0089218",
-            "locked": false,
-            "addedOn": "2021-12-29T19:55:45.247Z",
-            "watchedOn": null,
-            "ratings": {
-                "__typename": "Ratings",
-                "id": "66974fab-db07-41d9-8ecc-f4ceabd891f9",
-                "IMDB": "78%",
-                "ROTTEN_TOMATOES": "77%",
-                "METACRITIC": "62%"
-            },
-            "background": null
-        }
-    ],
-    "moviesById": {
-        "0b3c9190-f6d9-404b-904f-654a127a4ae7": {
-            "__typename": "Movie",
-            "id": "0b3c9190-f6d9-404b-904f-654a127a4ae7",
-            "title": "Star Wars: Episode IV - A New Hope",
-            "list": "5ea3b2ee-761b-47c8-abb9-9377f3826931",
-            "runtime": 7260,
-            "source": 6,
-            "genre": 3,
-            "year": "1977",
-            "poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-            "imdbID": "tt0076759",
-            "locked": false,
-            "addedOn": "2021-12-29T19:55:11.013Z",
-            "watchedOn": null,
-            "ratings": {
-                "__typename": "Ratings",
-                "id": "0b3c9190-f6d9-404b-904f-654a127a4ae7",
-                "IMDB": "86%",
-                "ROTTEN_TOMATOES": "92%",
-                "METACRITIC": "90%"
-            },
-            "background": null
-        },
-        "66974fab-db07-41d9-8ecc-f4ceabd891f9": {
-            "__typename": "Movie",
-            "id": "66974fab-db07-41d9-8ecc-f4ceabd891f9",
-            "title": "The Goonies",
-            "list": "5ea3b2ee-761b-47c8-abb9-9377f3826931",
-            "runtime": 6840,
-            "source": 0,
-            "genre": 1,
-            "year": "1985",
-            "poster": "https://m.media-amazon.com/images/M/MV5BODRlMjRkZGEtZWM2Zi00ZjYxLWE0MWUtMmM1YWM2NzZlOTE1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-            "imdbID": "tt0089218",
-            "locked": false,
-            "addedOn": "2021-12-29T19:55:45.247Z",
-            "watchedOn": null,
-            "ratings": {
-                "__typename": "Ratings",
-                "id": "66974fab-db07-41d9-8ecc-f4ceabd891f9",
-                "IMDB": "78%",
-                "ROTTEN_TOMATOES": "77%",
-                "METACRITIC": "62%"
-            },
-            "background": null
-        }
-    },
-    "watchedMovies": null,
-    "loadingMovies": false,
-    "order": [
-        "addedOn",
-        "desc"
-    ],
-    "pick": null
-}
-   */
   const context = {
     lists,
     list,
