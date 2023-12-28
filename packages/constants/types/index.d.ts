ffa66a6575c46fcc4402c3c8d886834c512c4674
Jason Saelhof <jsaelhof@gmail.com>
2023-12-27 22:31:15 -0700

    #150: Add generated types to constants package (not converted to TS yet)

---
+++ packages/constants/types/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./sources.js";
+export * from "./error-codes.js";
+export * from "./genres.js";
+export * from "./ratings.js";
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/constants/types/ratings.d.ts
@@ -0,0 +1,7 @@
+export const ratingsSource: Readonly<{
+    IMDB: "IMDB";
+    ROTTEN_TOMATOES: "ROTTEN_TOMATOES";
+    METACRITIC: "METACRITIC";
+}>;
+export const ratingsSources: ("IMDB" | "ROTTEN_TOMATOES" | "METACRITIC")[];
+//# sourceMappingURL=ratings.d.ts.map
